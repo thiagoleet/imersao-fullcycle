614417e45ca393c27144b309e7349ed6dc2d1831
Thiago Leite <thiagofmleite@gmail.com>
2023-08-24 13:47:54 -0300

    Melhorias no RoutesController/RoutesService

--- nestjs/src/routes/routes.service.ts
+++ nestjs/src/routes/routes.service.ts
@@ -1,36 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
-import {PrismaService} from 'src/prisma/prisma/prisma.service';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { DirectionsService } from 'src/maps/directions/directions.service';
 
 @Injectable()
 export class RoutesService {
-  constructor(private prismaService: PrismaService) {
+  constructor(
+    private prismaService: PrismaService,
+    private directionsService: DirectionsService,
+  ) {}
 
-  }
+  async create(createRouteDto: CreateRouteDto) {
+    const { available_travel_modes, geocoded_waypoints, routes, request } =
+      await this.directionsService.getDirections(
+        createRouteDto.source_id,
+        createRouteDto.destination_id,
+      );
 
-  create(createRouteDto: CreateRouteDto) {
+    const legs = routes[0].legs[0];
+
     return this.prismaService.route.create({
       data: {
         name: createRouteDto.name,
         source: {
-          name: createRouteDto.source_id,
+          name: legs.start_address,
           location: {
-              lat: 0,
-              lng: 0,
+            lat: legs.start_location.lat,
+            lng: legs.start_location.lng,
           },
         },
         destination: {
-          name: createRouteDto.destination_id,
+          name: legs.end_address,
           location: {
-              lat: 0,
-              lng: 0,
+            lat: legs.end_location.lat,
+            lng: legs.end_location.lng,
           },
         },
-        distance: 0,
-        duration: 0,
-        directions: "{}"
-      }
+        distance: legs.distance.value,
+        duration: legs.duration.value,
+        directions: JSON.stringify({
+          available_travel_modes,
+          geocoded_waypoints,
+          routes,
+          request,
+        }),
+      },
     });
   }
 
@@ -38,8 +53,10 @@ export class RoutesService {
     return this.prismaService.route.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} route`;
+  findOne(id: string) {
+    return this.prismaService.route.findUniqueOrThrow({
+      where: { id },
+    });
   }
 
   update(id: number, updateRouteDto: UpdateRouteDto) {
