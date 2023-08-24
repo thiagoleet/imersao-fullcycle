0f0b397ccb656bb0f3be9e1d318b7479c7d82774
Thiago Leite <thiagofmleite@gmail.com>
2023-08-24 15:35:25 -0300

    Adicionado RoutesDriver service

---
+++ nestjs/src/routes/dto/create-route-driver.dto.ts
@@ -0,0 +1,5 @@
+export class CreateRouteDriverDto {
+  route_id: string;
+  lat: number;
+  lng: number;
+}
---
+++ nestjs/src/routes/routes-driver/routes-driver.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { CreateRouteDriverDto } from '../dto/create-route-driver.dto';
+
+@Injectable()
+export class RoutesDriverService {
+  constructor(private prismaService: PrismaService) {}
+
+  async createOrUpdate(dto: CreateRouteDriverDto) {
+    // const countRouteDriver = await this.prismaService.routeDriver.count({
+    //   where: {
+    //     route_id: createRouteDriverDto.route_id,
+    //   },
+    // });
+
+    return await this.prismaService.routeDriver.upsert({
+      include: { route: true },
+      where: {
+        route_id: dto.route_id,
+      },
+      create: {
+        route_id: dto.route_id,
+        points: {
+          set: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lng,
+            },
+          },
+        },
+      },
+      update: {
+        points: {
+          push: {
+            location: {
+              lat: dto.lat,
+              lng: dto.lng,
+            },
+          },
+        },
+      },
+    });
+  }
+}
